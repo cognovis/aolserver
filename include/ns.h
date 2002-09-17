26db82cfbfea343794cb2f4802c9e768e0633fff
jgdavidson <>
2002-09-17 23:53:25 +0000

    Update defs for Tcl 8.4 compile.

--- include/ns.h
+++ include/ns.h
@@ -819,14 +819,14 @@ NS_EXTERN Ns_List *Ns_ListMapcar(Ns_List *lPtr, Ns_ElemValProc *valProc);
  * tclxkeylist.c:
  */
 
-NS_EXTERN char *Tcl_DeleteKeyedListField (Tcl_Interp  *interp, const char *fieldName,
-        const char *keyedList);
-NS_EXTERN int Tcl_GetKeyedListField (Tcl_Interp  *interp, const char *fieldName,
-        const char *keyedList, char **fieldValuePtr);
-NS_EXTERN int Tcl_GetKeyedListKeys (Tcl_Interp  *interp, char const *subFieldName,
-        const char *keyedList, int *keysArgcPtr, char ***keysArgvPtr);
-NS_EXTERN char *Tcl_SetKeyedListField (Tcl_Interp  *interp, const char *fieldName,
-        const char *fieldvalue, const char *keyedList);
+NS_EXTERN char *Tcl_DeleteKeyedListField (Tcl_Interp  *interp, CONST char *fieldName,
+        CONST char *keyedList);
+NS_EXTERN int Tcl_GetKeyedListField (Tcl_Interp  *interp, CONST char *fieldName,
+        CONST char *keyedList, char **fieldValuePtr);
+NS_EXTERN int Tcl_GetKeyedListKeys (Tcl_Interp  *interp, char CONST *subFieldName,
+        CONST char *keyedList, int *keysArgcPtr, char ***keysArgvPtr);
+NS_EXTERN char *Tcl_SetKeyedListField (Tcl_Interp  *interp, CONST char *fieldName,
+        CONST char *fieldvalue, CONST char *keyedList);
 
 /*
  * listen.c:
