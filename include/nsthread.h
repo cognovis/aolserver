815121be0f57f641390943da2bfae3fdc4862dd0
jgdavidson <>
2002-11-10 16:08:16 +0000

    Moved include <sys/types.h> ahead of <dirent.h> for OS/X.

--- include/nsthread.h
+++ include/nsthread.h
@@ -75,12 +75,12 @@
 #endif
 
 #ifndef WIN32
+#include <sys/types.h>
 #include <dirent.h>
 #include <sys/time.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netdb.h>
