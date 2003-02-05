6c241dd6f795da87fee40d7179d4e97ae05c28bb
cvs2svn <oct@openacs.org>
2003-02-05 14:31:58 +0000

    This commit was manufactured by cvs2svn to create tag 'aolserver_v35_b5'.
    Sprout from aolserver_v35_bp 2002-12-03 15:28:31 UTC schaudhri 'Changed tag to 3.5.2'
    Cherrypick from aolserver_v35_bp 2003-02-05 14:31:57 UTC elizthom 'nsext.c: Fix LocalProxy function to call Ns_CloseOnExec':
        nsd/tclvar.c
        ChangeLog
        nsext/nsext.c
        nsd/nsd.h
        nsd/nsmain.c
        tcl/sendmail.tcl
        nsd/tclmisc.c
        nsthread/winthread.c
    Delete:
        win32/README
        win32/aolserver.dsw
        win32/cgitest/cgitest.dsp
        win32/cgitest/main.c
        win32/main/main.dsp
        win32/nscgi/nscgi.dsp
        win32/nscp/nscp.dsp
        win32/nsd/nsd.dsp
        win32/nslog/nslog.dsp
        win32/nsperm/nsperm.dsp
        win32/nssock/nssock.dsp
        win32/nsthread/nsthread.dsp
        win32/test/nsd.tcl
        win32/test/servers/test/pages/cgitest.bat
        win32/test/servers/test/pages/index.adp
        win32/threadtest/threadtest.dsp

--- nsd/tclmisc.c
+++ nsd/tclmisc.c
@@ -87,6 +87,8 @@ static int JpegSize(Tcl_Channel chan, int *wPtr, int *hPtr);
  * Static variables defined in this file
  */
 
+static Ns_ThreadArgProc ThreadArgProc;
+
 
 /*
  *----------------------------------------------------------------------
@@ -156,8 +158,7 @@ NsIsIdConn(char *connId)
  *	1 if semi, 0 if space. 
  *
  * Side effects:
- *	Behavior is undefined if string ends before either space or 
- *	semi. 
+ *      Undefined behavior if string does not end in null
  *
  *----------------------------------------------------------------------
  */
@@ -165,7 +166,16 @@ NsIsIdConn(char *connId)
 static int
 WordEndsInSemi(char *ip)
 {
-    while((*ip != ' ') && (*ip != ';')) {
+    if (ip == NULL) {
+        return 0;
+    }
+    /* advance past the first '&' so we can check for a second 
+       (i.e. to handle "ben&jerry&nbsp;")
+    */
+    if (*ip == '&') {
+        ip++;
+    }
+    while((*ip != '\0') && (*ip != ' ') && (*ip != ';') && (*ip != '&')) {
         ip++;
     }
     if (*ip == ';') {
@@ -926,7 +936,7 @@ NsTclInfoCmd(ClientData dummy, Tcl_Interp *interp, int argc, char **argv)
 	Ns_MutexList(&ds);
 	Tcl_DStringResult(interp, &ds);
     } else if (STREQ(argv[1], "threads")) {
-	Ns_ThreadList(&ds, NULL);
+	Ns_ThreadList(&ds, ThreadArgProc);
 	Tcl_DStringResult(interp, &ds);
     } else if (STREQ(argv[1], "pools")) {
 #ifndef _WIN32
@@ -1729,4 +1739,10 @@ JpegSize(Tcl_Channel chan, int *wPtr, int *hPtr)
 	}
     }
     return TCL_ERROR;
+}
+
+static void      
+ThreadArgProc(Tcl_DString *dsPtr, void *proc, void *arg)
+{
+    Ns_GetProcInfo(dsPtr, proc, arg);
 }
